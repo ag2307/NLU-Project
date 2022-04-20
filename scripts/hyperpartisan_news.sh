e7fe724f76aaa501b73c16b9b69035a63ee9b53a
Shubhankar Ranade <shubhankar.ich@gmail.com>
2022-04-20 01:28:41 -0400

    Add scripts for training on Greene

---
+++ scripts/hyperpartisan_news.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source /ext3/miniconda3/etc/profile.d/conda.sh
+
+conda activate domains
+
+cd ${SCRATCH}/nlu-proj/NLU-Project/vendor/dont-stop-pretraining
+
+echo $(pwd)
+echo "Training on "$1" samples from hyperpartisan news"
+
+# need to manually download models in pretrained_models
+# not tracked on github
+
+python -m scripts.train \
+--config training_config/classifier.jsonnet \
+--serialization_dir model_logs/hyperpartisan-dapt \
+--hyperparameters ROBERTA_CLASSIFIER_SMALL \
+--dataset hyperpartisan \
+--train_throttle $1 \
+--model $(pwd)/pretrained_models/news_roberta_base \
+--device 0 \
+--perf +f1 \
+--evaluate_on_test
